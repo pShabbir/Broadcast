828e346a74dac96070894ac9ca1576d2c6acd4a1
Shabbir Hussain <pshabbir36@gmail.com>
2017-06-08 09:58:31 +0530

    Capable of reading and receiving sms

--- app/src/main/java/com/example/sh/broadcast/MainActivity.java
+++ app/src/main/java/com/example/sh/broadcast/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.sh.broadcast;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CheckUserPermsions();
     }
 
     public void buBroadcast(View view) {
@@ -18,5 +24,49 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction("com.example.Broadcast");
         intent.putExtra("msg","This is finally happening");
         sendBroadcast(intent);
+    }
+
+
+
+    //access to permsions
+    void CheckUserPermsions(){
+
+        if ( Build.VERSION.SDK_INT >= 23){
+            if( (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) !=
+                    PackageManager.PERMISSION_GRANTED  )&&
+                    (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) !=
+                            PackageManager.PERMISSION_GRANTED  ))
+                    {
+
+                requestPermissions(new String[]{
+                                Manifest.permission.READ_SMS},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return ;
+            }
+        }
+
+//        readContact();// init the contact list
+
+    }
+    //get acces to location permsion
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    readContact();// init the contact list
+                } else {
+                    // Permission Denied
+                    Toast.makeText( this,"your message" , Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
--- app/src/main/java/com/example/sh/broadcast/MyReceiver.java
+++ app/src/main/java/com/example/sh/broadcast/MyReceiver.java
@@ -1,9 +1,15 @@
 package com.example.sh.broadcast;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 /**
@@ -11,15 +17,42 @@ import android.widget.Toast;
  */
 
 public class MyReceiver extends BroadcastReceiver {
+
+
+    Context context;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Bundle b=intent.getExtras();
 
-        if(intent.getAction().equalsIgnoreCase("com.example.Broadcast"));
+        Bundle b = intent.getExtras();
+
+        if (intent.getAction().equalsIgnoreCase("com.example.Broadcast")) ;
         {
-                String msg = b.getString("msg");
-                Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+            String msg = b.getString("msg");
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
         }
+        if (intent.getAction().equalsIgnoreCase("android.provider.Telephony.SMS_RECEIVED")) {
+            if (b != null) {
+
+                final Object[] pdusObj = (Object[]) b.get("pdus");
+                SmsMessage[] messages = new SmsMessage[pdusObj.length];
+                for (int i = 0; i < messages.length; i++) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        String format = b.getString("format");
+                        messages[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i], format);
+                    } else {
+                        messages[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    }
+                    // SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String senderNum = messages[i].getOriginatingAddress();
+                    String message = messages[i].getMessageBody();//
+
+                    Toast.makeText(context, senderNum+" : "+message, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+
     }
 }
